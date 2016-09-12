2f54accf1e66832ee72617469eabf904312137b3
g012 <leclerc.pascal@gmail.com>
2016-09-13 00:18:52 +0200

    Added wav export.

--- main.c
+++ main.c
@@ -10,16 +10,15 @@ static size_t s_fwrite(void* ctx, const void* ptr, size_t size)
 int main(int argc, char** argv)
 {
     struct ofl_synth synth, *s = &synth;
-    s->samplingfreq = 44100.0f;
-    s->samplingfreq_i = 1.0f / s->samplingfreq;
-    s->buf_len = (int)s->samplingfreq * sizeof(*s->buf);
-    s->buf_off = 0;
-    s->buf = malloc(s->buf_len);
+    float samplingrate = 44100.0f;
+    size_t buf_sz = (size_t)samplingrate * sizeof(float);
+    void* buf = malloc(buf_sz);
+    ofl_synth_init(s, buf, buf_sz, samplingrate);
 
-    ofl_synth_osc_sin(s, s->buf_len / sizeof(*s->buf), 440.0f);
+    ofl_synth_osc_sin(s, s->buf_len, 440.0f);
 
-    FILE* f = fopen("test_mono_f32.raw", "wb");
-    ofl_synth_fwrite_raw(s, s_fwrite, f);
+    FILE* f = fopen("test.wav", "wb");
+    ofl_synth_fwrite_wav(s, s_fwrite, f);
     fclose(f);
 
     return 0;
--- ofl_synth.h
+++ ofl_synth.h
@@ -17,11 +17,12 @@ extern "C" {
 
 struct ofl_synth
 {
-    float samplingfreq, samplingfreq_i;
+    float samplingrate, samplingrate_i;
     int buf_len;
     int buf_off;
     float *buf;
 };
+OFL_SYNTH_DEF void ofl_synth_init(struct ofl_synth *s, float *buf, size_t buf_sz, float samplingrate);
 
 OFL_SYNTH_DEF void ofl_synth_osc_sin(struct ofl_synth *s, int count, float phase);
 
@@ -43,11 +44,21 @@ OFL_SYNTH_DEF void ofl_synth_fwrite_wav(struct ofl_synth *s, ofl_synth_fwrite fw
 
 #include <math.h>
 
-#define OFL_SYNTH_PI 3.14159265358979323846f
+#define OFL_SYNTH_PI (3.14159265358979323846f)
+#define OFL_SYNTH_2PI (6.28318530717958647693f)
+
+OFL_SYNTH_DEF void ofl_synth_init(struct ofl_synth *s, float *buf, size_t buf_sz, float samplingrate)
+{
+    s->samplingrate = samplingrate;
+    s->samplingrate_i = 1.0f / s->samplingrate;
+    s->buf_len = (int)(buf_sz / sizeof(*buf));
+    s->buf_off = 0;
+    s->buf = buf;
+}
 
 OFL_SYNTH_DEF void ofl_synth_osc_sin(struct ofl_synth *s, int count, float phase)
 {
-    float f = s->samplingfreq_i * OFL_SYNTH_PI * phase;
+    float f = s->samplingrate_i * OFL_SYNTH_2PI * phase;
     float *b = s->buf + s->buf_off;
     for (int i = 0; i < count; ++i) *b++ = sinf(i * f);
     s->buf_off += count;
@@ -55,11 +66,38 @@ OFL_SYNTH_DEF void ofl_synth_osc_sin(struct ofl_synth *s, int count, float phase
 
 OFL_SYNTH_DEF void ofl_synth_fwrite_raw(struct ofl_synth *s, ofl_synth_fwrite fw, void *ctx)
 {
-    fw(ctx, s->buf, s->buf_len);
+    fw(ctx, s->buf, s->buf_len * sizeof(*s->buf));
 }
 
 OFL_SYNTH_DEF void ofl_synth_fwrite_wav(struct ofl_synth *s, ofl_synth_fwrite fw, void *ctx)
 {
+    short b[2048];
+    struct wavfile_header
+    {
+        char	riff_tag[4];
+        int	    riff_length;
+        char	wave_tag[4];
+        char	fmt_tag[4];
+        int	    fmt_length;
+        short	audio_format;
+        short	num_channels;
+        int	    sample_rate;
+        int	    byte_rate;
+        short	block_align;
+        short	bits_per_sample;
+        char	data_tag[4];
+        int	    data_length;
+    } h = { "RIFF", 2*s->buf_len + sizeof(struct wavfile_header) - 8, "WAVE", "fmt ", 16, 1, 1, (int)s->samplingrate, 2*(int)s->samplingrate, 2, 16, "data", 2*s->buf_len };
+    fw(ctx, &h, sizeof(h));
+    float *f = s->buf;
+    for (int i = 0; i < s->buf_len; i += sizeof(b) / sizeof(*b))
+    {
+        int c = sizeof(b) / sizeof(*b);
+        if (c > s->buf_len - i) c = s->buf_len - i;
+        int cw = c;
+        for (short *d = b; c > 0; --c) *d++ = (short)(*f++ * 32767.0f);
+        fw(ctx, b, cw * sizeof(*b));
+    }
 }
 
 #endif // OFL_SYNTH_IMPLEMENTATION
